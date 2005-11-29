506463a715f8074b57e10fd99285720f92885f2e
riri <riri@4ec29174-9504-0410-90c6-c0d7d85f43c4>
2005-11-29 14:44:49 +0000

    real and good make recursive behaviour, manage: make {,build|clean|install|uninstall}

---
+++ website/plat.mk
@@ -0,0 +1,15 @@
+# Included make variables
+
+SRCDIR = src
+PROCDIR = proc
+OUTDIR = htdocs
+RESDIR = res
+
+XSLTPROC = xsltproc
+XSLTFLAGS = -nonet
+
+TIDY = tidy
+TIDYFLAGS = -asxml -i -utf8 -m -n
+
+FORCE:
+
